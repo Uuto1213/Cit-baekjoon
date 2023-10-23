4262b672f92dffe977b63b7708276fa82f769a8b
Uuto1213 <139743937+Uuto1213@users.noreply.github.com>
2023-10-23 20:01:39 +0900

    [Bronze III] Title: ABC, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3047. ABC/ABC.py
@@ -0,0 +1,10 @@
+a = list(map(int, input().split()))
+x = input()
+a.sort()
+for i in (x):
+    if i == "A":
+        print(a[0], end = " ")
+    elif i == "B":
+        print(a[1], end = " ")
+    else:
+        print(a[2], end = " ")
